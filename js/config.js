1b0279fb3947006238edff937aa96af307dab293
RenOmstead <renomstead@gmail.com>
2026-09-04 09:22:21 -0400

    Add initial configuration and starter data

--- js/config.js
+++ js/config.js
@@ -1,1 +1,33 @@
+/* =========================================================
+   SHELFMARK CONFIG
+   ========================================================= */
 
+
+window.SHELFMARK_CONFIG = {
+
+    productName:
+        "Shelfmark",
+
+    tagline:
+        "my little library",
+
+    environment:
+        "Portfolio Demo"
+
+};
+
+
+/* =========================================================
+   EMPTY STARTER DATA
+   ========================================================= */
+
+
+window.SHELFMARK_DATA = {
+
+    shelves:
+        [],
+
+    books:
+        []
+
+};
